8d5e9ddc563b0cceb911492dad8dfa019457922d
lizbliss <elizabeth.blissett@gmail.com>
2017-04-26 18:27:01 -0400

    Adding files

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of our friends.
+// ===============================================================================
+
+var friends = [{
+    name: "Asher",
+    photo: "http://www.carolinaherrera.com/wp-content/uploads/2016/02/CH-Men-Prive_listing2-1400x700.jpg"
+    scores: [""]
+}, {
+    name: "Mona",
+    photo: "http://media.new.mensxp.com/media/content/2016/Mar/these-are-the-worlds-most-beautiful-women-in-the-world-according-to-gamers-652x400-1-1459331481.jpg",
+    scores: [""]
+}];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
